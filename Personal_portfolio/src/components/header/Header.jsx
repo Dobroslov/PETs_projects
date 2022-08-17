0dce58626f353742ba27a216761d597aed09320c
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2022-08-17 17:29:03 +0300

    create html and css header

--- Personal_portfolio/src/components/header/Header.jsx
+++ Personal_portfolio/src/components/header/Header.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
 
-import './header.css'
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Vladimir
+        </a>
+        <div className="nav_menu">
+          <ul className="nav_list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav__icon"> Home</i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"> About</i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"> Skills</i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"> Service</i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"> Portfolio</i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"> Contact</i>
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times close"></i>
+        </div>
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
