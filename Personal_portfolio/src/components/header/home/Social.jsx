3d5ba003b49628b1289e9a94f6d00bb304be11ec
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2022-08-18 21:23:08 +0300

    start section home

---
+++ Personal_portfolio/src/components/header/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a
+        href='https://www.linkedin.com/in/vladimir-arshinnikov-388853223/'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-linkedin-alt'></i>
+      </a>
+      <a
+        href='https://github.com/Dobroslov'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-github-alt'></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
